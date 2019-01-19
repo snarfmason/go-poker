dac7a2e744b74f9c1e5bc9265efdaf5f3a6c46ce
Jon Mason <jonathan.e.mason@gmail.com>
2019-01-19 11:11:17 -0800

    Make main func output in the ruby-poker format.
    For now it always uses the first hand as the winner and has rank none.

--- main/main.go
+++ main/main.go
@@ -1,30 +1,44 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "os"
-  "strings"
-  "go-poker/poker"
+	"bufio"
+	"fmt"
+	"go-poker/poker"
+	"os"
+	"strings"
 )
 
 func main() {
-  reader := bufio.NewReader(os.Stdin)
-  line, err := reader.ReadString('\n')
-  round := 0
-  for err == nil {
-    strs := strings.Split(line, "|")
-    n_hands := len(strs)
-    hands := make([]poker.Hand, n_hands)
-    for i := 0; i < n_hands; i++ {
-      hands[i] = poker.ParseHand(strs[i])
-    }
+	reader := bufio.NewReader(os.Stdin)
+	line, err := reader.ReadString('\n')
+	for err == nil {
+		hands := hands(line)
 
-    round++
-    fmt.Println("\nRound", round)
-    for i := 0; i < n_hands; i++ {
-      fmt.Println(hands[i])
-    }
-    line, err = reader.ReadString('\n')
-  }
+		winners := winners(hands)
+
+		output := fmt.Sprintf(
+			"%v, Winner: %v, Rank: %v",
+			strings.TrimRight(line, "\n"),
+			strings.Join(winners, ", "),
+			"none") // winners[0].RankString()
+
+		fmt.Println(output)
+		line, err = reader.ReadString('\n')
+	}
+}
+
+func hands(line string) []poker.Hand {
+	strs := strings.Split(line, "|")
+	n_hands := len(strs)
+	hands := make([]poker.Hand, n_hands)
+	for i := 0; i < n_hands; i++ {
+		hands[i] = poker.ParseHand(strs[i])
+	}
+	return hands
+}
+
+func winners(hands []poker.Hand) []string {
+	winners := make([]string, 0)
+	// just return the first hand for now
+	return append(winners, hands[0].String())
 }
